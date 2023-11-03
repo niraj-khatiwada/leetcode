a2a9ef20b1647eb08492e078927ea5514a97f758
niraj-khatiwada <nirazzz.nk@gmail.com>
2023-11-03 11:24:41 +0545

    switch group anagrams solution to performant hashKey method

--- AnagramGrouping/Main.java
+++ AnagramGrouping/Main.java
@@ -1,15 +1,58 @@
+//package AnagramGrouping;
+//
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.HashMap;
+//import java.util.List;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        String[] anagrams = {"eat", "tea", "tan", "ate", "nat", "bat", "eat"};
+//        System.out.println(groupAnagrams(anagrams));
+//
+//        HashMap<Integer, String> map = new HashMap<>();
+//    }
+//
+//    public static List<List<String>> groupAnagrams(String[] strs) {
+//        List<List<String>> result = new ArrayList<>();
+//
+//        if (strs.length <= 1) {
+//            result.add(List.of(strs));
+//            return result;
+//        }
+//        HashMap<String, List<String>> map = new HashMap<>();
+//
+//        for (var word : strs) {
+//            char[] charArray = word.toCharArray();
+//            Arrays.sort(charArray);
+//            var key = new String(charArray);
+//            var list = map.containsKey(key) ? map.get(key) : new ArrayList<String>();
+//            list.add(word);
+//            map.put(key, list);
+//        }
+//        for (var entry : map.entrySet()) {
+//            result.add(entry.getValue());
+//        }
+//        return result;
+//    }
+//}
+//
+//
+
 package AnagramGrouping;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        String[] anagrams = {"eat", "tea", "tan", "ate", "nat", "bat", "eat"};
+//        String[] anagrams = {"eat", "tea", "tan", "ate", "nat", "bat", "eat"};
+        String[] anagrams = {"", ""};
         System.out.println(groupAnagrams(anagrams));
+
     }
 
     public static List<List<String>> groupAnagrams(String[] strs) {
@@ -20,18 +63,20 @@ public class Main {
             return result;
         }
         HashMap<String, List<String>> map = new HashMap<>();
-
         for (var word : strs) {
-            char[] charArray = word.toCharArray();
-            Arrays.sort(charArray);
-            var key = new String(charArray);
-            var list = map.containsKey(key) ? map.get(key) : new ArrayList<String>();
+            char[] count = new char[26];
+            for (var c : word.toCharArray()) {
+                count[c - 'a']++;
+            }
+            var hashKey = new String(count);
+            var list = map.containsKey(hashKey) ? map.get(hashKey) : new ArrayList<String>();
             list.add(word);
-            map.put(key, list);
+            map.put(hashKey, list);
         }
         for (var entry : map.entrySet()) {
             result.add(entry.getValue());
         }
+
         return result;
     }
 }
