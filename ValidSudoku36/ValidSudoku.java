9934ca9edc3ebd6709cf2b509fcc836a9cca1d09
niraj-khatiwada <nirazzz.nk@gmail.com>
2024-07-28 11:50:53 +0545

    Valid sudoku using hash map method

--- ValidSudoku36/ValidSudoku.java
+++ ValidSudoku36/ValidSudoku.java
@@ -1,10 +1,60 @@
 package ValidSudoku36;
 
 import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 // Medium
 public class ValidSudoku {
 
+    // Using HashMap Method
+    // O(n^2). Only looped once. Highly efficient
+    // But this takes extra spaces for 3 hash maps
+    public boolean isValidSudokuUsingHashMapMethod(char[][] board) {
+        Map<String, Set<Character>> rowMap = new HashMap<>();
+        Map<String, Set<Character>> columnMap = new HashMap<>();
+        Map<String, Set<Character>> gridMap = new HashMap<>();
+
+        for (var i = 0; i < 9; i++) {
+            for (var j = 0; j < 9; j++) {
+                var value = board[i][j];
+                if (value == '.') {
+                    continue;
+                }
+                // Rows
+                var rowKey = String.format("%s", i);
+                var rowSet = rowMap.get(rowKey) == null ? new HashSet<Character>() : rowMap.get(rowKey);
+                if (!rowSet.add(value)) {
+                    return false;
+                }
+                rowMap.put(rowKey, rowSet);
+
+                // Columns
+                var columnKey = String.format("%s", j);
+                var columnSet = columnMap.get(columnKey) == null ? new HashSet<Character>()
+                        : columnMap.get(columnKey);
+                if (!columnSet.add(value)) {
+                    return false;
+                }
+                columnMap.put(columnKey, columnSet);
+
+                // Grid
+                // We interpolate the higher matrix into smaller one
+                var gridKey = String.format("%s|%s", (i / 3), (j / 3));
+                var gridSet = gridMap.get(gridKey) == null ? new HashSet<Character>() : gridMap.get(gridKey);
+                if (!gridSet.add(value)) {
+                    return false;
+                }
+                gridMap.put(gridKey, gridSet);
+            }
+        }
+        return true;
+    }
+
+    // O(n^2) * 3 ~ O(n^2)
+    // You need to do nested operation three times so less efficient
+    // No extra spaces is allocated though
     public boolean isValidSudoku(char[][] board) {
         // Rows
         for (var i = 0; i < 9; i++) {
@@ -35,7 +85,7 @@ public class ValidSudoku {
         // Sub-grid
         var x = 0;
         var y = 0;
-        while (x > 8) {
+        while (x < 9) {
             var set = new HashSet<Character>();
             for (var i = x; i < (x + 3); i++) {
                 for (var j = y; j < (y + 3); j++) {
