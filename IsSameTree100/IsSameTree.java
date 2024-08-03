1eeb0ee5b7513ddb4afcb1148fd0dc28b46d0573
niraj-khatiwada <nirazzz.nk@gmail.com>
2024-08-03 17:05:15 +0545

    Is Same binary Tree

---
+++ IsSameTree100/IsSameTree.java
@@ -0,0 +1,33 @@
+package IsSameTree100;
+
+public class IsSameTree {
+    private class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public Node root;
+
+    public boolean isSameTree(Node p, Node q) {
+        return this._isSameTree(p, q);
+    }
+
+    private boolean _isSameTree(Node p, Node q) {
+        if (p == null) {
+            return q == null;
+        }
+        if (q == null) {
+            return p == null;
+        }
+        if (p.value != q.value) {
+            return false;
+        }
+
+        return _isSameTree(p.left, q.left) && _isSameTree(p.right, q.right);
+    }
+}
