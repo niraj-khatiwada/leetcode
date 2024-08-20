15f7fe735ed4fa21153b9c9c3445913e0adfe378
niraj-khatiwada <nirazzz.nk@gmail.com>
2024-08-20 21:05:44 +0545

    Min cost climbing stairs using DP

--- MinCostClimbingStairs746/MinCostClimbingStairs.java
+++ MinCostClimbingStairs746/MinCostClimbingStairs.java
@@ -20,4 +20,12 @@ public class MinCostClimbingStairs {
         dp[i] = min - v;
         return min;
     }
+
+    // Tabulation DP
+    public int minCostClimbingStairsDP(int[] cost) {
+        for (var i = (cost.length - 2); i >= 0; i--) {
+            cost[i] += Math.min(cost[i + 1], (i + 2) >= cost.length ? 0 : cost[i + 2]);
+        }
+        return Math.min(cost[0], cost[1]);
+    }
 }
