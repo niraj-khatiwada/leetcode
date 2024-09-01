3db221332bb4179e717603abba05c4c75bc14056
niraj-khatiwada <nirazzz.nk@gmail.com>
2024-09-01 22:14:25 +0545

    Search a 2D matrix

---
+++ SearchA2DMatrix74/SearchA2DMatrix.java
@@ -0,0 +1,30 @@
+package SearchA2DMatrix74;
+
+public class SearchA2DMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        var flatten = new int[matrix.length * matrix[0].length];
+        for (var i = 0; i < matrix.length; i++) {
+            for (var j = 0; j < matrix[0].length; j++) {
+                var ij = i * matrix[0].length + j;
+                flatten[ij] = matrix[i][j];
+            }
+        }
+        return _binarySearch(flatten, target);
+    }
+
+    private boolean _binarySearch(int[] array, int target) {
+        var l = 0;
+        var r = array.length - 1;
+        while (l <= r) {
+            var m = (r + l) / 2;
+            if (array[m] == target) {
+                return true;
+            } else if (target < array[m]) {
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+        return false;
+    }
+}
