9ae668a587c41f4e17c714a45f859f30901cde30
niraj-khatiwada <nirazzz.nk@gmail.com>
2024-09-07 18:09:02 +0545

    Course Schedule II

--- CourseScheduleII210/CourseScheduleII.java
+++ CourseScheduleII210/CourseScheduleII.java
@@ -1,10 +1,11 @@
-package CourseSchedule207;
+package CourseScheduleII210;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Stack;
 
-public class CourseSchedule {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
+public class CourseScheduleII {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
         Set<Integer>[] edges = new HashSet[numCourses];
         for (var i = 0; i < numCourses; i++) {
             edges[i] = new HashSet<Integer>();
@@ -13,16 +14,24 @@ public class CourseSchedule {
             edges[p[1]].add(p[0]);
         }
         var visited = new HashSet<Integer>();
+        var stack = new Stack<Integer>();
         for (var i = 0; i < numCourses; i++) {
             var visiting = new HashSet<Integer>();
-            if (!this._dfs(edges, i, visiting, visited)) {
-                return false;
+            if (!this._dfs(edges, i, visiting, visited, stack)) {
+                return new int[0];
             }
         }
-        return true;
+        var order = new int[numCourses];
+        var p = 0;
+        while (!stack.isEmpty()) {
+            order[p] = stack.pop();
+            p++;
+        }
+        return order;
     }
 
-    public boolean _dfs(Set<Integer>[] edges, int i, Set<Integer> visiting, Set<Integer> visited) {
+    public boolean _dfs(Set<Integer>[] edges, int i, Set<Integer> visiting, Set<Integer> visited,
+            Stack<Integer> stack) {
         if (i >= edges.length || visited.contains(i)) {
             return true;
         }
@@ -32,11 +41,12 @@ public class CourseSchedule {
         }
         visiting.add(i);
         for (var edge : edges[i]) {
-            if (!_dfs(edges, edge, visiting, visited)) {
+            if (!_dfs(edges, edge, visiting, visited, stack)) {
                 return false;
             }
         }
         visited.add(i);
+        stack.add(i);
         visiting.remove(i);
         return true;
     }
