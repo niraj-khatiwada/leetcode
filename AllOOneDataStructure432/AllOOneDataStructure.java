33e0a99c78e6a9e1d4ff20f67e054405787ca9ae
niraj-khatiwada <nirazzz.nk@gmail.com>
2024-09-29 23:20:23 +0545

    All O(1) Data Structires

--- AllOOneDataStructure432/AllOOneDataStructure.java
+++ AllOOneDataStructure432/AllOOneDataStructure.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
 
+// NOTE: All operations run in O(log(n)) not in O(1)
 public class AllOOneDataStructure {
     public TreeMap<Integer, Set<String>> sortedMap = new TreeMap<>();
     public Map<String, Integer> map = new HashMap<>();
